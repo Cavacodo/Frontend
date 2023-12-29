30a76fb529c1a8c4a625d23348fa9044ebb13461
chennaji <2078634206@qq.com>
2023-12-29 15:36:18 +0800

    Hii

---
+++ gaokao/src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import commonMenu from "./components/CommonMenu.vue";
+</script>
+<template>
+  <div class="common-layout">
+    <el-container>
+      <el-header>
+        <common-menu></common-menu>
+      </el-header>
+      <el-main>
+        <router-view />
+      </el-main>
+      <el-footer>Footer</el-footer>
+    </el-container>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  name: "App",
+  components: {},
+};
+</script>
+
+<style>
+html,
+body,
+#app {
+  margin: 0;
+  padding: 0;
+  /* height: 100%; */
+}
+html {
+  overflow-y: scroll;
+}
+</style>
