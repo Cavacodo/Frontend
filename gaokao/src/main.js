82d0c9dbf185ef45142974a57d1b70fde9d11945
ALEX-VII <15091076006@163.com>
2023-12-29 17:03:56 +0800

    sb

--- gaokao/src/main.js
+++ gaokao/src/main.js
@@ -3,13 +3,13 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import ElementPlus from 'element-plus'
-import Vue from 'vue'
-import ElementUI from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
+// import Vue from 'vue'
+// import ElementUI from 'element-ui'
+// import 'element-ui/lib/theme-chalk/index.css'
 
-Vue.use(ElementUI)
 import 'element-plus/dist/index.css'
 import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+// Vue.use(ElementUI)
 const app = createApp(App)
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
     app.component(key, component)
