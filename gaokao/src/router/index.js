4b17b1da8c04be85ed592980e4816f474313e36b
ALEX-VII <15091076006@163.com>
2024-01-03 11:35:01 +0800

    速速上传

--- gaokao/src/router/index.js
+++ gaokao/src/router/index.js
@@ -1,95 +1,14 @@
 import {
-    createRouter,
-    createWebHashHistory
+  createRouter,
+  createWebHashHistory
 } from 'vue-router'
 
 // import store from '../store/index.js'
 
 const routes = [{
-    path: '/',
-    redirect: '/home'
+  path: '/',
+  redirect: '/home'
 },
-<<<<<<< HEAD
-    {
-        path: '/login',
-        name: 'login',
-        component: () => import('../views/LoginView.vue'),
-        meta: {
-            title: '登录|高考推荐数据库'
-        }
-    },
-    {
-        path: '/home',
-        component: () => import('../views/HomeView.vue'),
-    },
-    {
-        path: '/school',
-        name: 'school',
-        component: () => import('../views/SchoolView.vue'),
-        meta: {
-            need2Login: 1,
-            title: '学校查询|高考推荐数据库'
-        }
-    },
-    {
-        path: '/special',
-        name: 'special',
-        component: () => import('../views/SpecialView.vue'),
-        meta: {
-            need2Login: 1,
-            title: '专业查询|高考推荐数据库'
-        }
-    },
-    {
-        path: '/recommend',
-        name: 'recommend',
-        component: () => import('../views/RecommendView.vue'),
-        meta: {
-            need2Login: 1,
-            title: '报考推荐|高考推荐数据库'
-        }
-    },
-    {
-        path: '/my',
-        name: 'my',
-        component: () => import('../views/MyView.vue'),
-        meta: {
-            title: '学校详情|高考推荐数据库'
-        }
-    },
-    {
-        path: '/analyse',
-        name: 'collegeAnalysis',
-        component: () => import('../views/CollegeAnalysis.vue'),
-        meta: {
-            title: '院校分析'
-        },
-    },
-    {
-        path: '/predict',
-        name: 'scorePredict',
-        component: () => import('../views/ScorePrediction.vue'),
-        meta: {
-            title: '分数预测'
-        }
-    },
-    {
-        path: '/bigdata',
-        name: 'bigdata',
-        component: () => import('../views/BigDataShow.vue'),
-        meta: {
-            title: '数据大屏'
-        }
-    },
-    {
-        path: '/TestMock',
-        name: 'testTry',
-        component: () => import('../views/TestMock.vue'),
-        meta: {
-            title: 'testTry'
-        }
-    }
-=======
 {
   path: '/login',
   name: 'login',
@@ -153,21 +72,29 @@ const routes = [{
     title: '分数预测'
   }
 },
+// {
+//   path: '/bigdata',
+//   name: 'bigdata',
+//   component: () => import('../views/BigDataShow.vue'),
+//   meta: {
+//     title: '数据大屏'
+//   }
+// },
 {
   path: '/TestMock',
   name: 'testTry',
   component: () => import('../views/TestMock.vue'),
-  meta:{
+  meta: {
     title: 'testTry'
   }
 }
->>>>>>> aed0e48 (我操你们妈)
+
 ]
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    //mode: 'history',
-    routes
+  history: createWebHashHistory(),
+  //mode: 'history',
+  routes
 })
 
 
