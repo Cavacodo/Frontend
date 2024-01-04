772e99e8379ceed5f273fe996ab5e58d376b96af
chennaji <2078634206@qq.com>
2024-01-04 15:31:15 +0800

    改头换面

--- gaokao/src/views/MyProfile.vue
+++ gaokao/src/views/MyProfile.vue
@@ -1,15 +1,4 @@
 <template>
-  <el-upload
-    class="avatar-uploader"
-    action="https://run.mocky.io/v3/9d059bf9-4660-45f2-925d-ce80ad6c4d15"
-    :show-file-list="false"
-    :on-success="handleAvatarSuccess"
-    :before-upload="beforeAvatarUpload"
-  >
-    <img v-if="imageUrl" :src="imageUrl" class="avatar" />
-    <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
-  </el-upload>
-
   <div style="margin: 20px 0" />
   <div>
     <el-card class="box2" style="width: 250px;">
@@ -133,31 +122,7 @@ import {
 } from '@element-plus/icons-vue'
 import { useRoute, useRouter } from "vue-router";
 import { ref } from 'vue'
-///////////////////////////
-import { ElMessage } from 'element-plus'
-import { Plus } from '@element-plus/icons-vue'
-import type { UploadProps } from 'element-plus'
 
-const imageUrl = ref('')
-
-const handleAvatarSuccess: UploadProps['onSuccess'] = (
-  response,
-  uploadFile
-) => {
-  imageUrl.value = URL.createObjectURL(uploadFile.raw!)
-}
-
-const beforeAvatarUpload: UploadProps['beforeUpload'] = (rawFile) => {
-  if (rawFile.type !== 'image/jpeg') {
-    ElMessage.error('Avatar picture must be JPG format!')
-    return false
-  } 
-  // else if (rawFile.size / 1024 / 1024 > 2) {
-  //   ElMessage.error('Avatar picture size can not exceed 2MB!')
-  //   return false
-  // }
-  return true
-}
 ////////////////////////////
 const text = ref('')
 const text1 = ref('')
